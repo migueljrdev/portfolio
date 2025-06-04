4c44e0ef04d2533d67c4c2745b2b7f657c6a5060
Miguel Jr <miguel.conta.jr@gmail.com>
2025-06-04 01:04:44 -0300

    add menu fixed, scroll horizontal e section background-text

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,23 @@ imgAvatar.addEventListener('mouseover', () => {
 imgAvatar.addEventListener('mouseout', () => {
   imgAvatar.src = "assets/img/Avatar.png";
 });
+
+
+const scrollContainer = document.getElementById('scrollContainer');
+
+// Converte scroll vertical para horizontal
+scrollContainer.addEventListener('wheel', (e) => {
+// Comportamento natural de scroll horizontal
+  if (e.deltaY !== 0) {
+    e.preventDefault(); // Evita o scroll vertical
+    scrollContainer.scrollBy({
+      left: e.deltaY,
+      behavior: 'smooth'
+    });
+  }
+}, { passive: false });
+
+function scrollToSection(id) {
+  const section = document.getElementById(id);
+  section.scrollIntoView({ behavior: 'smooth', inline: 'start' });
+}
