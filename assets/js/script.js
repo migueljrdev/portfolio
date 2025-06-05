ec872992ca3044d8dec1e3e2464c87421367b3db
Miguel Jr <miguel.conta.jr@gmail.com>
2025-06-05 02:06:31 -0300

    add marcador de link ativo no nav e alteraçao de background atraves do id

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,70 @@ function scrollToSection(id) {
   const section = document.getElementById(id);
   section.scrollIntoView({ behavior: 'smooth', inline: 'start' });
 }
+
+// Função para scroll suave e destaque
+function scrollToSection(id) {
+    const section = document.getElementById(id);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+        setActiveLink(id);
+    }
+}
+
+// Função para marcar link ativo
+function setActiveLink(id) {
+    const links = document.querySelectorAll('.li-nav');
+    links.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${id}`) {
+            link.classList.add('active');
+        }
+    });
+}
+
+// Detecta rolagem e marca o link correto
+
+scrollContainer.addEventListener("scroll", () => {
+    console.log("scroll ativado");
+
+    const sectionDivs = scrollContainer.querySelectorAll(".conteiner > div[id]");
+    const scrollPos = scrollContainer.scrollLeft + scrollContainer.clientWidth / 2;
+
+    let currentId = null;
+
+    sectionDivs.forEach(section => {
+        const left = section.offsetLeft;
+        const width = section.offsetWidth;
+        if (scrollPos >= left && scrollPos < left + width) {
+            currentId = section.id;
+        }
+    });
+
+    if (currentId) {
+        setActiveLink(currentId);
+        updateBackgroundOverlay(currentId);
+    }
+});
+
+
+//mudar o background de acordo com a section
+function updateBackgroundOverlay(sectionId) {
+    const overlay = document.querySelector('.background-overlay');
+
+    switch (sectionId) {
+        case 'sobre-mim':
+            overlay.style.backgroundImage = "url('/assets/img/pagina1.png')";
+            break;
+        case 'projetos':
+            overlay.style.backgroundImage = "url('/assets/img/pag2.png')";
+            break;
+        case 'skills':
+            overlay.style.backgroundImage = "url('/assets/img/pag2.png')";
+            break;
+        case 'contato':
+            overlay.style.backgroundImage = "url('/assets/img/pag3.png')";
+            break;
+        default:
+            overlay.style.backgroundImage = "none";
+    }
+}
